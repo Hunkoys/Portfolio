13c3407a5daa465b743b851fe84fc1e5478f0655
hunkoys <hunkyminik@gmail.com>
2021-07-14 02:23:41 -0700

    Image alignments fixed

--- script/layout/screens/goal.js
+++ script/layout/screens/goal.js
@@ -10,7 +10,7 @@ const start = CircleFrame('pics/face.jpg', 'Profile Picture', DEFAULT, 0, -40).s
   left: '335px',
   marginBottom: '30px',
 });
-const end = CircleFrame('pics/nanadaime.gif', 'Bad Ass Person', 470, -100).style({
+const end = CircleFrame('pics/nanadaime.gif', 'Bad Ass Person', 400, -80).style({
   position: 'relative',
   top: '-150px',
   left: '780px',
@@ -20,14 +20,16 @@ const end = CircleFrame('pics/nanadaime.gif', 'Bad Ass Person', 470, -100).style
 const path = dom.svg({
   width: 888,
   height: 928,
-  content: {
-    type: 'path',
-    d: 'M443.876 4C443.876 106.92 181.544 4 181.544 125.299C181.544 246.598 330.719 201.641 416.782 189.199C554.666 169.266 574.245 177.32 649.789 189.199C725.333 201.078 824.589 231.524 866.539 320.959C922.64 440.561 833.071 555.688 746.37 566.949C659.67 578.21 525.158 533.302 380.445 510.965C235.731 488.628 4.00008 503.896 4 682.593C3.99992 870.62 166.882 912.75 283.544 921.797C400.207 930.845 691.865 909.639 763.902 901.157',
-    stroke: '#3F3F3F',
-    'stroke-width': '8',
-    'stroke-linecap': 'round',
-    'stroke-dasharray': '20 40',
-  },
+  contents: [
+    {
+      type: 'path',
+      d: 'M443.876 4C443.876 106.92 181.544 4 181.544 125.299C181.544 246.598 330.719 201.641 416.782 189.199C554.666 169.266 574.245 177.32 649.789 189.199C725.333 201.078 824.589 231.524 866.539 320.959C922.64 440.561 833.071 555.688 746.37 566.949C659.67 578.21 525.158 533.302 380.445 510.965C235.731 488.628 4.00008 503.896 4 682.593C3.99992 870.62 166.882 912.75 283.544 921.797C400.207 930.845 691.865 909.639 763.902 901.157',
+      stroke: '#3F3F3F',
+      'stroke-width': '8',
+      'stroke-linecap': 'round',
+      'stroke-dasharray': '20 40',
+    },
+  ],
 });
 
 const masterCard = Card(
